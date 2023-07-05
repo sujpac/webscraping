33e30fa1a7c51ba07224e9a53a603c41bd9d1b73
Sujit Packiaraj <sujit.pack@gmail.com>
2023-07-05 13:24:54 -0700

    dump to an example json file

--- bs4_script.py
+++ bs4_script.py
@@ -1,6 +1,7 @@
 import requests
 from collections import Counter
 from bs4 import BeautifulSoup
+import json
 
 response = requests.get("https://news.ycombinator.com/")
 if response.status_code != 200:
@@ -9,13 +10,10 @@ if response.status_code != 200:
 
 soup = BeautifulSoup(response.content, 'html.parser')
 print(soup.title)
-
 print(soup.title.string)
 
 num_links = len(soup.find_all('a'))
 print(f"There are {num_links} links in this page")
-
-print(soup.get_text())
 
 first_link = soup.a
 print(first_link)
@@ -33,12 +31,39 @@ top3_links = Counter(all_hrefs).most_common(3)
 print(top3_links)
 
 # Extract only the article links from the main page
+# NOTE: This doesn't work, there is no more "titlelink" class, find another
+# way to grab just the article links
 
 
 def my_tag_selector(tag):
-    # TODO: This doesn't work, there is no more titlelink class, find another
-    # way to grab just the article links
     return tag.name == "a" and tag.has_attr("class") and "titlelink" in tag.get("class")
 
 
 print(soup.find_all(my_tag_selector))
+
+# Loop through each article and print its text contents to the
+html = response.text
+soup = BeautifulSoup(html)
+articles = soup.find_all(class_='athing')
+
+for article in articles:
+    print(article.text)
+
+# Extract each article's URL, title, and rank
+
+output = []
+
+for article in articles:
+    data = {
+        'URL': article.find(class_='titleline').find('a').get('href'),
+        'title': article.find(class_='titleline').getText(),
+        'rank': article.find(class_='rank').getText().replace('.', '')
+    }
+    output.append(data)
+
+print(output)
+
+print('Saving output data to JSON file.')
+save_output = open('hn_data.json', 'w')
+json.dump(output, save_output, indent=6, ensure_ascii=False)
+save_output.close()
